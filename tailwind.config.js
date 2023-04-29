1b44dcf495e1ed6bb8da2afd327148da8871967a
ANUSREEPURUSHOTHAMANstartupmission <121005558+ANUSREEPURUSHOTHAMANstartupmission@users.noreply.github.com>
2023-04-29 17:17:33 +0530

    changes

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./src/**/*.{html,js}'],
+  theme: {
+    
+    extend: {
+      backgroundImage: {
+        'hero': "url('../../img/AdobeStock_184033815 (1).jpg')",
+
+      },
+    },
+  },
+  plugins: [],
+}
+
